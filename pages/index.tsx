595ca6725ca9e82f927805d66c016f7a5daecfd5
nano <nano@nanonoMacBook-Air.local>
2023-06-15 22:18:02 +0900

    エディタ画面の作成

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,8 +3,9 @@ import Header from "@/components/Header/header";
 export default function Home() {
   return (
       <>
-        <h1>だだだ</h1>
-        
+          <div className="block  bg-white border border-gray-200 rounded-lg shadow  dark:border-gray-700 m-auto my-5 h-[600px] w-[1200px]">
+              <div className="block  bg-white border border-gray-200 rounded-lg shadow  p-5 w-10"></div>
+          </div>
       </>
   );
 }
