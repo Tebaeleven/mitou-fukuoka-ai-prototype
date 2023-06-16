1595c41e6c3dcb30465be4a7b7ce03753d41816e
nano <nano@nanonoMacBook-Air.local>
2023-06-16 19:23:54 +0900

    headerのリンクの状態を保持するように改良

--- components/Header/header.tsx
+++ components/Header/header.tsx
@@ -7,11 +7,34 @@ import Button from "@mui/material/Button";
 import IconButton from "@mui/material/IconButton";
 import MenuIcon from "@mui/icons-material/Menu";
 import Link from "next/link";
+import { useRouter } from "next/router";
 
 export default function Header() {
+    const CustomLink = ({ href, title}) => {
+        const router = useRouter();
+        return (
+            <Link
+                href={href}
+                className={`hover:text-primary-700  hover:text-white border-gray-700 relative group 
+                    ${
+                        router.asPath === href ? "text-white" : "text-gray-400 "
+                    }`}
+            >
+                {title}
+                <span
+                    className={`
+                        h-[3px] inline-block bg-white absolute left-0 -bottom-2 group-hover:w-full transition-[width] ease duration-700
+                        ${router.asPath === href ? "w-full" : "w-0"}
+                    `}
+                >
+                    &nbsp;
+                </span>
+            </Link>
+        );
+    };
     return (
         <header>
-            <nav className="bg-white border-gray-200 px-4 lg:px-6 py-2.5 dark:bg-gray-800">
+            <nav className=" border-gray-200 px-4 lg:px-6 py-2.5 bg-gray-800">
                 <div className="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl">
                     <Link href="/" className="flex items-center">
                         {/* <img
@@ -19,27 +42,27 @@ export default function Header() {
                             className="mr-3 h-6 sm:h-9"
                             alt="Flowbite Logo"
                         /> */}
-                        <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white">
+                        <span className="self-center text-xl font-semibold whitespace-nowrap text-white">
                             未踏福岡プロトタイプ
                         </span>
                     </Link>
                     <div className="flex items-center lg:order-2">
                         <a
                             href="#"
-                            className="text-gray-800 dark:text-white hover:bg-gray-50 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 dark:hover:bg-gray-700 focus:outline-none dark:focus:ring-gray-800"
+                            className="text-gray-800 dark:text-white  focus:ring-4  font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 hover:bg-gray-700 focus:outline-none focus:ring-gray-800"
                         >
-                            Log in
+                            ログイン
                         </a>
                         <a
                             href="#"
-                            className="text-white bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 dark:bg-primary-600 dark:hover:bg-primary-700 focus:outline-none dark:focus:ring-primary-800"
+                            className="text-gray-800 dark:text-white  focus:ring-4  font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 hover:bg-gray-700 focus:outline-none focus:ring-gray-800"
                         >
-                            Get started
+                            新規登録
                         </a>
                         <button
                             data-collapse-toggle="mobile-menu-2"
                             type="button"
-                            className="inline-flex items-center p-2 ml-1 text-sm text-gray-500 rounded-lg lg:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+                            className="inline-flex items-center p-2 ml-1 text-sm  rounded-lg lg:hidden  focus:outline-none focus:ring-2 ring-gray-200 text-gray-400 hover:bg-gray-700 focus:ring-gray-600"
                             aria-controls="mobile-menu-2"
                             aria-expanded="false"
                         >
@@ -75,55 +98,15 @@ export default function Header() {
                         id="mobile-menu-2"
                     >
                         <ul className="flex flex-col mt-4 font-medium lg:flex-row lg:space-x-8 lg:mt-0">
-                            <li>
-                                <a
-                                    href="#"
-                                    className="block py-2 pr-4 pl-3 text-white rounded bg-primary-700 lg:bg-transparent lg:text-primary-700 lg:p-0 dark:text-white"
-                                    aria-current="page"
-                                >
-                                    Home
-                                </a>
-                            </li>
-                            <li>
-                                <a
-                                    href="#"
-                                    className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-primary-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700"
-                                >
-                                    Company
-                                </a>
-                            </li>
-                            <li>
-                                <a
-                                    href="#"
-                                    className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-primary-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700"
-                                >
-                                    Marketplace
-                                </a>
-                            </li>
-                            <li>
-                                <a
-                                    href="#"
-                                    className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-primary-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700"
-                                >
-                                    Features
-                                </a>
-                            </li>
-                            <li>
-                                <a
-                                    href="#"
-                                    className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-primary-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700"
-                                >
-                                    Team
-                                </a>
-                            </li>
-                            <li>
-                                <a
-                                    href="#"
-                                    className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-primary-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700"
-                                >
-                                    Contact
-                                </a>
-                            </li>
+                            <CustomLink href="/" title="Home" />
+                            <CustomLink href="/about" title="About" />
+                            <CustomLink
+                                href="/Marketplace"
+                                title="Marketplace"
+                            />
+                            <CustomLink href="/Features" title="Features" />
+                            <CustomLink href="/Team" title="Team" />
+                            <CustomLink href="/contact" title="Contact" />
                         </ul>
                     </div>
                 </div>
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,11 @@
-import Image from 'next/image'
+import Image from "next/image";
 import Header from "@/components/Header/header";
 export default function Home() {
-  return (
-      <>
-          <div className="block  bg-white border border-gray-200 rounded-lg shadow  dark:border-gray-700 m-auto my-5 h-[600px] w-[1200px]">
-              <div className="block  bg-white border border-gray-200 rounded-lg shadow  p-5 w-10"></div>
-          </div>
-      </>
-  );
+    return (
+        <>
+            <div className="block  bg-white border border-gray-200 rounded-lg shadow  dark:border-gray-700 m-auto my-5 h-[600px] w-[1200px]">
+                <div className="block  bg-white border border-gray-200 rounded-lg shadow  p-5 w-10"></div>
+            </div>
+        </>
+    );
 }
